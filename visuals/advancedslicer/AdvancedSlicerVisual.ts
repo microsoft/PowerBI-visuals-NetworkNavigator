d820b7313be2cfeba839b29f81f37c817c450f77
Dario Segura <dsegura@uncharted.software>
2016-03-17 15:38:34 -0400

    [1428] Added tests to avoid errors when no data is present.

--- visuals/advancedslicer/AdvancedSlicerVisual.ts
+++ visuals/advancedslicer/AdvancedSlicerVisual.ts
@@ -131,7 +131,7 @@ export default class AdvancedSlicerVisual extends VisualBase implements IVisual 
         if (this.dataView) {
             var categorical = this.dataView && this.dataView.categorical;
             var newData = AdvancedSlicerVisual.converter(this.dataView, this.selectionManager);
-            if (this.loadDeferred) {
+            if (this.loadDeferred && this.mySlicer.data) {
 
                 let added = [];
                 let anyRemoved = false;
@@ -150,7 +150,7 @@ export default class AdvancedSlicerVisual extends VisualBase implements IVisual 
                 // We only need to give it the new items
                 this.loadDeferred.resolve(added);
                 delete this.loadDeferred;
-            } else if (Utils.hasDataChanged(newData.slice(0), this.mySlicer.data, (a, b) => a.match === b.match && a.renderedValue === b.renderedValue)) {
+            } else if (newData && Utils.hasDataChanged(newData.slice(0), this.mySlicer.data, (a, b) => a.match === b.match && a.renderedValue === b.renderedValue)) {
                 this.mySlicer.data = newData;
             }
             this.mySlicer.showValues = !!categorical && !!categorical.values && categorical.values.length > 0;
