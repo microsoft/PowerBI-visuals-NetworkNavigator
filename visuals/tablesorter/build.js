406fa2f5c78526d4d262e35673de59ff4d55695c
David Tittsworth <stopyoukid@gmail.com>
2016-03-21 13:43:41 -0700

    Updated the descriptions of the components, Fixes #1463

--- visuals/tablesorter/build.js
+++ visuals/tablesorter/build.js
@@ -13,7 +13,7 @@ module.exports = {
             "screenshot": "screenshot.png",
             "thumbnail": "thumbnail.png",
             "entry": "TableSorterVisual",
-            "description": "Table Sorter lets you create stacked table columns to explore how different combinations and weightings of numerical column values result in different rank orderings of table records. Column headings show the distribution of column values and support rapid re-sorting of table rows (which may also be filtered by linked visuals). Table Sorter is built on LineUp (http://caleydo.github.io/tools/lineup/)."
+            "description": "This visual is currently in beta testing and is undergoing active development. Table Sorter lets you create stacked table columns to explore how different combinations and weightings of numerical column values result in different rank orderings of table records. Column headings show the distribution of column values and support rapid re-sorting of table rows (which may also be filtered by linked visuals). Table Sorter is built on LineUp (http://caleydo.github.io/tools/lineup/)."
         },
         "component": {
             "entry": "TableSorter"
