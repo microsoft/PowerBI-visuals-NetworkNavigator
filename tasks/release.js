0096c49cb78e30dd256b2bfd39f36a8498a0aa12
David Tittsworth <stopyoukid@gmail.com>
2016-03-23 13:11:28 -0700

    Updated release to to everything

--- tasks/release.js
+++ tasks/release.js
@@ -29,39 +29,72 @@ module.exports = function(gulp) {
                 // bump the version number in those files 
                 .pipe(bump({type: importance}))
                 // save it back to filesystem 
-                .pipe(gulp.dest('./'))
-                
-                // Add our build file to be commited
-                .pipe(addsrc(paths.projectDir + "/build.js"))
-                
-                // commit the changed version number 
-                .pipe(git.commit('Bump versions'))
+                .pipe(gulp.dest('./'));
+        }
         
-                // read only one file to get the version number 
-                .pipe(filter('package.json'))
-                // **tag it in the repository** 
-                .pipe(tag_version());
+        function release(importance, cb) {
+            return sequence(`bump:${project}:${importance}`, `build:${project}`, `commit_artifacts:${project}`, `tag:${project}`, cb);
         }
         
         /**
          * Bumps the version of the project
          */
-        gulp.task(`release:${project}:patch`, function() {
+        gulp.task(`bump:${project}:patch`, function() {
             return inc('patch');
         });
         
         /**
          * Bumps the version of the project
          */
-        gulp.task(`release:${project}:minor`, function() {
+        gulp.task(`bump:${project}:minor`, function() {
             return inc('minor');
         });
         
         /**
          * Bumps the version of the project
          */
-        gulp.task(`release:${project}:major`, function() {
+        gulp.task(`bump:${project}:major`, function() {
             return inc('major');
+        });
+        
+        /**
+         * Commits the dist of the given project
+         */
+        gulp.task(`commit_artifacts:${project}`, function() {
+            return gulp.src([paths.buildDir, paths.projectDir + "/build.js", "package.json"])
+                
+                // Add the build artifacts
+                .pipe(git.commit('Updating Artifacts'));
+        });
+        
+        /**
+         * Tags the dist of the given project
+         */
+        gulp.task(`tag:${project}`, function() {
+            return gulp.src(['./package.json'])
+                // **tag it in the repository** 
+                .pipe(tag_version());;
+        });
+        
+        /**
+         * Releases the version of the project
+         */
+        gulp.task(`release:${project}:patch`, function(cb) {
+            return release("patch", cb);
+        });
+        
+        /**
+         * Releases the version of the project
+         */
+        gulp.task(`release:${project}:minor`, function(cb) {
+            return release("minor", cb);
+        });
+        
+        /**
+         * Releases the version of the project
+         */
+        gulp.task(`release:${project}:major`, function(cb) {
+            return release("major", cb);
         });
     });
 };
