ee4e4cab4fa961d5cf1e7cbad0f6295bffef5fea
darthtrevino <darthtrevino@gmail.com>
2016-04-05 17:20:03 -0700

    Use path.resolve in paths.ts instead of string-jamming

--- tasks/paths.ts
+++ tasks/paths.ts
@@ -1,22 +1,20 @@
-const rootDir = __dirname + "/../";
+import * as path from "path";
+const rootDir = path.join(__dirname, "..");
+const srcDir = path.join(rootDir, "src");
+const distDir = path.join(rootDir, "dist");
+const pbiDir = path.join(distDir, 'powerbi');
+
 module.exports = function() {
-    var srcDir = rootDir + 'src/';
     return  {
         projectDir: srcDir,
-        scripts: [srcDir + '/**/*\.{js,ts}'],
-        styles: [srcDir + '/**/*\.{scss,sass}'],
-        test: [srcDir + '/**/*\.spec\.{ts,js}'],
-        buildDir:  rootDir + 'dist/',
-        buildArtifacts:  rootDir + 'dist/' + '/**/*.*',
-        getBuildDir: function(type) {
-            return rootDir + 'dist/' + type;
-        },
-        buildDirComponentCSS:  rootDir + 'dist/' + "/component",
-        buildDirComponentJS:  rootDir + 'dist/' + "/component",
-        buildDirReactCSS:  rootDir + 'dist/' + "/react",
-        buildDirReactJS:  rootDir + 'dist/' + "/react",
-        buildDirPowerBI: rootDir + 'dist/' + '/powerbi',
-        buildDirPowerBiResources: rootDir + 'dist/' + '/powerbi/resources',
-        packageDir: [__dirname + '/build/package']
+        scripts: [`${srcDir}/**/*\.{js,ts}`],
+        styles: [`${srcDir}/**/*\.{scss,sass}`],
+        test: [`${srcDir}/**/*\.spec\.{ts,js}`],
+        buildDir:  distDir,
+        buildArtifacts: `${distDir}/**/*.*`,
+        getBuildDir: (type = '') => path.join(distDir, type),
+        buildDirPowerBI: pbiDir,
+        buildDirPowerBiResources: path.join(pbiDir, 'resources'),
+        packageDir: [path.join(__dirname, 'build/package')]
     };
 }
