78f208fa145cb4951a737ef1fac5025d49bee1eb
David Tittsworth <stopyoukid@gmail.com>
2017-06-19 10:38:40 -0700

    Added the compile file

---
+++ packages/network-navigator-react/bin/compile.js
@@ -0,0 +1,25 @@
+const exec = require('child_process').exec;
+const { spawn } = require('child_process');
+const isWin = /^win/.test(process.platform);
+const baseExec = `${__dirname}/../node_modules/.bin/tsc`;
+const tsc = isWin ?
+    spawn(`${baseExec}.cmd`) :
+    spawn('sh', [baseExec]);
+
+let hasErrors = false;
+function checkAndLog(data) {
+    data = (data || "") + "";
+    if (data.match(/\w/g) && data.indexOf("node_modules") < 0) {
+        console.log(data);
+        hasErrors = true;
+    }
+}
+
+tsc.stdout.on('data', checkAndLog);
+tsc.stderr.on('data', checkAndLog);
+
+tsc.on('close', (code) => {
+    if (hasErrors) {
+        process.exit(code);
+    }
+});
